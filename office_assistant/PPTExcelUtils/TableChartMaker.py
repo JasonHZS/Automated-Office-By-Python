8e64f6a5838c7133448158a43c51aec814c43f44
huangzesen <haungzesen@xgd.com>
2020-08-28 17:27:10 +0800

    excel2ppt.py

--- office_assistant/PPTExcelUtils/TableChartMaker.py
+++ office_assistant/PPTExcelUtils/TableChartMaker.py
@@ -15,19 +15,18 @@ plt.rcParams['font.sans-serif'] = ['SimHei']
 plt.rcParams['font.family'] = 'sans-serif'
 # 将df中的科学计数法数值格式化为保留5位小数
 pd.set_option('display.float_format', lambda x: '%.5f' % x)
-
 # pd.set_option('display.max_columns', None)  #  全部显示科学计数法
 # plt.style.use('ggplot')
 
 
 def sign_data(ax, type, x, y, format):
     """
-
-    :param ax:  图表对象
-    :param type: 'num'或'rate'
-    :param x: x轴数据list
-    :param y: y轴数据list
-    :param format: 数据格式（例如保留一位小数的百分数：'%.0f%%'，或者保留一位小数点：'%.1f'）
+    数据标注
+    :param ax:       图表对象
+    :param type:     'num'或'rate'
+    :param x:        x轴数据list
+    :param y:        y轴数据list
+    :param format:   数据格式（例如保留一位小数的百分数：'%.0f%%'，或者保留一位小数点：'%.1f'）
     :return:
     """
     for a, b in zip(x, y):
@@ -45,42 +44,40 @@ class Sheet4(object):
         self.sheet = 3
 
     def c_barline_trend(self):
-        """
-        sheet3对应数据生成柱形图与折线图：大POS新增入网商户数趋势
-        :return:
-        """
-        df = pd.read_excel(self.path, sheet_name=self.sheet, skiprows=22, skip_footer=38)
-        df1 = df.iloc[3:15, [0,1]]  # 柱形图
-        df2 = df.iloc[3:15, [0,2]]  # 折线图
+        df = pd.read_excel(self.path, sheet_name=self.sheet, skiprows=22, skipfooter=101)
+        df1 = df.iloc[3:15, [0, 1]]  # 柱形图
+        df2 = df.iloc[3:15, [0, 2]]  # 折线图
         df2['增长率'] = df2['增长率'] * 100
         month = df1['月份'].astype('str').values
-        # df1['新增商户数'] = df1['新增商户数'].apply(lambda x: "{:.1f}".format(float(x)))
         num = df1['新增商户数'].values
         rate = df2['增长率'].values
 
         fig, ax = plt.subplots(figsize=(14, 7))
         plt.grid(axis="y")  # 打开网格
         ax.set_axisbelow(True)  # 设置柱形图在网格上层
-        width = 0.5
-        ax.bar(month, num, width, label='新增商户数')
+
+        ax.bar(month, num, width=0.5, label='新增商户数')
         sign_data(ax, 'num', month, num, '%.1f')
         ax2 = ax.twinx()  # 设置共用x轴
         ax2.plot(month, rate, label='增长率', marker='.', color='orange')
         sign_data(ax2, 'rate', month, rate, '%.0f%%')
 
-        ax.set_title('大POS新增入网商户数趋势', fontsize=17)
-        fig.legend(loc='upper right')
+        ax.set_title('大POS新增入网商户数趋势', fontsize=20)
+        # 设置图例在图表底部
+        # box = ax.get_position()
+        # ax.set_position([box.x0, box.y0, box.width, box.height * 0.9])
+        ax.legend(loc='lower right', bbox_to_anchor=(0.5, -0.15), ncol=3, frameon=False, fontsize=13)
+        ax2.legend(loc='lower left', bbox_to_anchor=(0.5, -0.15), ncol=3, frameon=False, fontsize=13)
+
         plt.text(0.1, 480, '单位：万', fontsize=12)
+        ax.tick_params(labelsize=14)  # 修改ax对象xy轴字体
+        ax2.tick_params(labelsize=14)  # 修改ax2对象xy轴字体
         plt.savefig("D:/项目文档/分析报告/pic/大POS新增入网商户数趋势.jpg", dpi=500)
         # plt.show()
 
     def c_barline_activity(self):
-        """
-        sheet3对应数据生成柱形图与折线图：大POS商户活跃度
-        :return:
-        """
-        df = pd.read_excel(self.path, sheet_name=self.sheet, skiprows=51, skip_footer=68)
-        df1 = df.iloc[3:15, [0,1,2,3,4]]
+        df = pd.read_excel(self.path, sheet_name=self.sheet, skiprows=51, skipfooter=72)
+        df1 = df.iloc[3:15, [0, 1, 2, 3, 4]]
 
         month = df1['月份'].astype('str').values
         num_old = df1['老商户活跃数'].values
@@ -108,27 +105,22 @@ class Sheet4(object):
 
         yticks = mtick.FormatStrFormatter(fmt='%.2f%%')  # 设置百分比形式的坐标轴
         ax2.yaxis.set_major_formatter(yticks)
-        # 设置标注
-        box = ax.get_position()
-        ax.set_position([box.x0, box.y0, box.width, box.height * 0.9])
-        ax.legend(loc='lower center', bbox_to_anchor=(0.5, 1.15), ncol=3, frameon=False, fontsize=13)
-        ax2.legend(loc='lower center', bbox_to_anchor=(0.5, 1.1), ncol=3, frameon=False, fontsize=13)
+        # 设置图例在图表底部
+        ax.legend(loc='lower right', bbox_to_anchor=(0.5, -0.15), ncol=3, frameon=False, fontsize=13)
+        ax2.legend(loc='lower left', bbox_to_anchor=(0.5, -0.15), ncol=3, frameon=False, fontsize=13)
 
         ax.set_title('大POS商户活跃度', fontsize=19)
         plt.text(0.1, 75, '单位：万', fontsize=12)
+        ax.tick_params(labelsize=14)
+        ax2.tick_params(labelsize=14)
         plt.savefig("D:/项目文档/分析报告/pic/大POS商户活跃度.jpg", dpi=500)
         # plt.show()
 
     def c_pie_merchant(self):
-        """
-        sheet3对应数据生成饼图
-        :return:
-        """
-        df = pd.read_excel(self.path, sheet_name=self.sheet, skiprows=40, skip_footer=45)
+        df = pd.read_excel(self.path, sheet_name=self.sheet, skiprows=40, skipfooter=95)
         df1 = df.iloc[0:2, [0, 2]].set_index('新增类别')
         df2 = df.iloc[0:3, [3, 5]].set_index('存量类型')
-        df1.plot(kind='pie', autopct='%.0f%%', fontsize=15, subplots=True, explode=(0.01, 0.01), startangle=90,
-                 legend=False, figsize=(7, 7))
+        df1.plot(kind='pie', autopct='%.0f%%', fontsize=15, subplots=True, explode=(0.01, 0.01), startangle=90, legend=False, figsize=(7, 7))
         plt.title('3月大POS新增入网商户分布', size=25)
         plt.savefig("D:/项目文档/分析报告/pic/3月大POS新增入网商户分布.jpg", dpi=500)
         df2.plot(kind='pie', autopct='%.0f%%', fontsize=15, subplots=True, explode=(0.01, 0.01, 0.01), startangle=90,
@@ -144,8 +136,8 @@ class Sheet3(object):
         self.sheet = 2
 
     def c_barline_amount(self):
-        df = pd.read_excel(self.path, sheet_name=self.sheet, skiprows=1)
-        df1 = df.iloc[3:15, [0,1,2,3]]
+        df = pd.read_excel(self.path, sheet_name=self.sheet, skiprows=1, skipfooter=97)
+        df1 = df.iloc[3:15, [0, 1, 2, 3]]
 
         month = df1['月份'].astype('str').values
         old_mer_amo = df1['老商户交易金额'].values
@@ -165,19 +157,20 @@ class Sheet3(object):
         ax2 = ax.twinx()  # 设置共用x轴
         ax2.plot(month, rate, label='增长率', marker='.', color='orange')
         sign_data(ax2, 'rate', month, rate, '%.0f%%')
+
         yticks = mtick.FormatStrFormatter(fmt='%.1f%%')  # 设置百分比形式的坐标轴
         ax2.yaxis.set_major_formatter(yticks)
         ax.set_title('大POS交易金额趋势图', fontsize=19)
-        box = ax.get_position()
-        ax.set_position([box.x0, box.y0, box.width, box.height * 0.9])
-        ax.legend(loc='lower center', bbox_to_anchor=(0.5, 1.15), ncol=3, frameon=False, fontsize=13)
-        ax2.legend(loc='lower center', bbox_to_anchor=(0.5, 1.1), ncol=3, frameon=False, fontsize=13)
+        ax.legend(loc='lower right', bbox_to_anchor=(0.5, -0.15), ncol=3, frameon=False, fontsize=13)
+        ax2.legend(loc='lower left', bbox_to_anchor=(0.5, -0.15), ncol=3, frameon=False, fontsize=13)
         plt.text(0.01, 180, '单位：亿元', fontsize=12)
+        ax.tick_params(labelsize=14)
+        ax2.tick_params(labelsize=14)
         plt.savefig("D:/项目文档/分析报告/pic/大POS交易金额趋势图.jpg", dpi=500)
         # plt.show()
 
     def c_ring_pnums(self):
-        df = pd.read_excel(self.path, sheet_name=self.sheet, skiprows=55)
+        df = pd.read_excel(self.path, sheet_name=self.sheet, skiprows=55, skipfooter=48)
         df1 = df.iloc[0:7, [0, 2]].set_index(keys='交易类型')
         df2 = df.iloc[8:10, [0, 2]].set_index('交易类型')
         explode = (0.04, 0.01, 0.01, 0.02, 0.05, 0.1, 0.01)
@@ -202,7 +195,7 @@ class Sheet3(object):
         return df1[0:3].index.values, df1[0:3].values
 
     def c_ring_pamount(self):
-        df = pd.read_excel(self.path, sheet_name=self.sheet, skiprows=72)
+        df = pd.read_excel(self.path, sheet_name=self.sheet, skiprows=72, skipfooter=32)
         df1 = df.iloc[0:7, [0, 1]].set_index('交易类型')
         # df1['金额占比'] = df1['金额占比'].apply(lambda x: "{:.2%}".format(float(x)))
         df2 = df.iloc[7:9, [0, 1]].set_index('交易类型')
@@ -229,14 +222,14 @@ class Sheet1(object):
         self.sheet = 0
 
     def t_profit_ranking(self):
-        df = pd.read_excel(self.path, sheet_name=self.sheet, skiprows=19, skip_footer=31)
+        df = pd.read_excel(self.path, sheet_name=self.sheet, skiprows=19, skipfooter=32)
         df1 = df.iloc[0:11, [0, 1, 2, 3, 4]]
         # 将该列格式化（加上‘+’或‘-’）输出,':+.0f'的对象必须是浮点数
         df1['Unnamed: 1'][1:11] = df1['Unnamed: 1'][1:11].apply(lambda x: "{:+.0f}".format(float(x)) if x != '-' else x)
         return df1, df1.shape[0], df1.shape[1]
 
     def t_bpos_profit(self):
-        df = pd.read_excel(self.path, sheet_name=self.sheet, skiprows=34)
+        df = pd.read_excel(self.path, sheet_name=self.sheet, skiprows=34, skipfooter=14)
         df1 = df.iloc[0:14, [0, 1, 2, 3, 4, 5, 6, 7, 8]]
         df1['Unnamed: 1'][1:14] = df1['Unnamed: 1'][1:14].apply(lambda x: "{:.0f}".format(float(x)))
         df1['Unnamed: 2'][1:14] = df1['Unnamed: 2'][1:14].apply(lambda x: "{:.0f}".format(float(x)))
@@ -257,12 +250,12 @@ class Sheet1(object):
         df1 = df.iloc[0:3, [12, 13]].set_index(keys='当月收益分布')
         explode = (0.01, 0.01, 0.01)
         df1.plot(kind='pie', autopct='%.0f%%', fontsize=15, subplots=True, explode=explode, startangle=90, legend=False, figsize=(7, 7))
-        df2 = df.iloc[[0,15], [4,5,6,7,8]]
+        df2 = df.iloc[[0, 15], [4, 5, 6, 7, 8]]
         plt.savefig("D:/项目文档/分析报告/pic/3月收益分布.jpg", dpi=500)
         return df1, df2
 
     def c_barline_profit(self):
-        df = pd.read_excel(self.path, sheet_name=self.sheet, skiprows=1)
+        df = pd.read_excel(self.path, sheet_name=self.sheet, skiprows=1, skipfooter=46)
         df1 = df.iloc[3:15, [0, 1, 2, 3, 4, 5, 6]]  # 柱形图——收益
 
         month = df1['月份'].astype('str').values
@@ -276,7 +269,7 @@ class Sheet1(object):
         withdraw_rate = df1['提现收益增长率'].values
         server_rate = df1['服务费收益增长率'].values
 
-        fig, ax = plt.subplots(figsize=(10, 5))
+        fig, ax = plt.subplots(figsize=(14, 7))
         plt.grid(axis="y")  # 打开网格
         ax.set_axisbelow(True)  # 设置柱形图在网格上层
         ax.set_title('大POS收益趋势图', fontsize=19)
@@ -296,10 +289,9 @@ class Sheet1(object):
 
         yticks = mtick.FormatStrFormatter(fmt='%.1f%%')  # 设置百分比形式的坐标轴
         ax2.yaxis.set_major_formatter(yticks)
-        plt.tick_params(labelsize=14)
-        box = ax.get_position()
-        ax.set_position([box.x0, box.y0, box.width, box.height * 0.9])
-        ax.legend(loc='lower center', bbox_to_anchor=(0.5, 1.15), ncol=3, frameon=False, fontsize=13)
-        ax2.legend(loc='lower center', bbox_to_anchor=(0.5, 1.1), ncol=3, frameon=False, fontsize=13)
+        ax.tick_params(labelsize=14)
+        ax2.tick_params(labelsize=14)
+        ax.legend(loc='lower right', bbox_to_anchor=(0.5, -0.15), ncol=3, frameon=False, fontsize=13)
+        ax2.legend(loc='lower left', bbox_to_anchor=(0.5, -0.15), ncol=3, frameon=False, fontsize=13)
         plt.savefig("D:/项目文档/分析报告/pic/大POS收益趋势图.jpg", dpi=500)
         # plt.show()
